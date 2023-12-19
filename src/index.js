b1f7ca19096bca77b00d0474ebd703f6654b504c
evofan <evofan@outlook.jp>
2023-12-19 18:59:12 +0900

    Add tiker

--- src/index.js
+++ src/index.js
@@ -51,8 +51,8 @@ let text1 = new PIXI.Text(temp, {
 });
 container.addChild(text1);
 text1.anchor.set(0.5);
-text1.x = WIDTH / 2;
-text1.y = HEIGHT / 2;
+text1.x = WIDTH / 2 - 40;
+text1.y = HEIGHT / 2 - 10;
 
 let temp2 = `${PIXI.VERSION}`;
 console.log(temp2);
@@ -63,14 +63,17 @@ let text2 = new PIXI.Text(temp2, {
 });
 container.addChild(text2);
 text2.anchor.set(0.5);
-text2.x = WIDTH / 2 + text1.width - 10;
-text2.y = HEIGHT / 2;
+text2.x = WIDTH / 2 + text1.width - 50;
+text2.y = HEIGHT / 2 - 10;
+
+let image1;
+let loadingEnd = false;
 
 // Load image and Set sprite
 const LoadImg = async () => {
     console.log("LoadImg()");
     const texture1 = await Assets.load('assets/images/pic_cat2.png');
-    const image1 = Sprite.from(texture1);
+    image1 = Sprite.from(texture1);
     console.log(texture1); // Texture {_events: Events, _eventsCount: 0, noFrame: true, baseTexture: _BaseTexture, _frame: Rectangle, …}
     console.log(image1); // Sprite {_events: Events, _eventsCount: 0, tempDisplayObjectParent: null, transform: _Transform, alpha: 1, …}
     image1.anchor.set(0.5);
@@ -91,10 +94,26 @@ console.log("dice no: ", dice);
 
 const next = () => {
     console.log("next()");
+    // loading end flag
+    loadingEnd = true;
+
 }
 
 // view todays date
 let today = displayDateText(app);
+
+// Ticler
+app.ticker.add(() => {
+
+    console.log("tick...");
+    if (loadingEnd) {
+        image1.x = image1.x + 1;
+        if (image1.x >= WIDTH + image1.width / 2) {
+            image1.x = -image1.width / 2;
+        }
+    }
+
+});
 
 
 
